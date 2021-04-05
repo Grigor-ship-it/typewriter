2be9a026547f020acf4aa10f6fc265ec2b29750b
Grigor Baltadjian <gb795@hotmail.com>
2021-04-05 23:41:11 +0000

    typewriter

---
+++ typewriter.js
@@ -0,0 +1,8 @@
+const sentence = "hello there from lighthouse labs\n";
+let counter = 0
+for (const char of sentence) {
+  
+  setTimeout(() => {
+    process.stdout.write(char)}, counter);
+    counter = counter + 50
+}
